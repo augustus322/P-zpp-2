0bdc449594c2e97c88612889fc3393b0ae002707
augustus322 <127135490+augustus322@users.noreply.github.com>
2024-06-03 19:43:23 +0200

    UserManagerService - add methods to controller

---
+++ Microservices/UserManagerService/Controllers/UserManagementController.cs
@@ -0,0 +1,52 @@
+﻿using DatabaseService.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json;
+
+namespace UserManagerService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserManagementController : ControllerBase
+{
+    const string baseAddress = "DatabaseService";
+
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public UserManagementController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    [HttpGet]
+    public async Task<IQueryable<Employee>> GetUsers()
+    {
+        var httpClient = _httpClientFactory.CreateClient();
+        var response = await httpClient.GetAsync(baseAddress+"/EmployeeController/GetAll");
+
+        var list = JsonConvert.DeserializeObject<IQueryable<Employee>>(response.Content.ToString());
+        return list;
+    }
+
+    [HttpPost]
+    public void AddUser([FromBody] HttpContent content)
+    {
+        
+        var httpClient = _httpClientFactory.CreateClient();
+        httpClient.PostAsync(baseAddress + "/EmployeeController/Add", content);
+    }
+
+    [HttpPut("{id}")]
+    public void EditUser([FromBody] HttpContent content)
+    {
+        var httpClient = _httpClientFactory.CreateClient();
+        httpClient.PutAsync(baseAddress + "/EmployeeController/Add", content);
+    }
+
+    [HttpDelete("{id}")]
+    public void DeleteUser()
+    {
+        var httpClient = _httpClientFactory.CreateClient();
+        httpClient.DeleteAsync(baseAddress + "/EmployeeController/GetAll");
+    }
+}
---
+++ Microservices/UserManagerService/Model/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace DatabaseService.Model;
+
+public enum EmployeePosition
+{
+
+}
+public class Employee
+{
+    public int ID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Phone { get; set; }
+    public string Mail { get; set; }
+    public string Address { get; set; }
+    public EmployeePosition Position { get; set; }
+}
