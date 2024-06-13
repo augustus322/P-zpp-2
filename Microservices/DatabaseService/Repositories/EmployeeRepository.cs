b1c0aeab982490177a0be63c91c79b22284135b3
dawid200272 <45143323+dawid200272@users.noreply.github.com>
2024-06-13 11:17:14 +0200

    Update employee repository
    Updated employee repository to include other models in get methods (EF equivalent for JOIN in SQL)

--- Microservices/DatabaseService/Repositories/EmployeeRepository.cs
+++ Microservices/DatabaseService/Repositories/EmployeeRepository.cs
@@ -4,17 +4,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatabaseService.Repositories
 {
-    public class EmployeeRepository : IRepository<Employee>
-    {
-        private readonly HrDBContext _context;
+	public class EmployeeRepository : IRepository<Employee>
+	{
+		private readonly HrDBContext _context;
 
-        public EmployeeRepository(HrDBContext context)
-        {
-            _context = context;
-        }
+		public EmployeeRepository(HrDBContext context)
+		{
+			_context = context;
+		}
 
-        public async Task<Employee?> Add(Employee entity)
-        {
+		public async Task<Employee?> Add(Employee entity)
+		{
 			var result = await _context.AddAsync(entity);
 
 			var createdEntity = result.Entity;
@@ -22,39 +22,47 @@ namespace DatabaseService.Repositories
 			return createdEntity;
 		}
 
-        public async Task DeleteAsync(int id)
-        {
-            Employee? entity = await GetByIdAsync(id);
-            if (entity != null)
-            {
-                _context.Employees.Remove(entity);
-            }
-        }
+		public async Task DeleteAsync(int id)
+		{
+			Employee? entity = await GetByIdAsync(id);
+			if (entity != null)
+			{
+				_context.Employees.Remove(entity);
+			}
+		}
 
-        public IQueryable<Employee> GetAll()
-        {
-            return _context.Employees
+		public IQueryable<Employee> GetAll()
+		{
+			return _context.Employees
 				.Include(e => e.Recruitments)
-				.ThenInclude(r => r.Candidate)
+					.ThenInclude(r => r.Candidate)
+				.Include(e => e.TimeOffs)
+				.Include(e => e.Salaries)
+				.Include(e => e.Courses)
+				.Include(e => e.Meetings)
 				.AsQueryable();
-        }
+		}
 
-        public async Task<Employee?> GetByIdAsync(int id)
-        {
+		public async Task<Employee?> GetByIdAsync(int id)
+		{
 			return await _context.Employees
 				.Include(e => e.Recruitments)
-				.ThenInclude(r => r.Candidate)
+					.ThenInclude(r => r.Candidate)
+				.Include(e => e.TimeOffs)
+				.Include(e => e.Salaries)
+				.Include(e => e.Courses)
+				.Include(e => e.Meetings)
 				.FirstOrDefaultAsync(e => e.ID == id);
-        }
+		}
 
-        public async Task SaveAsync()
-        {
-            await _context.SaveChangesAsync();
-        }
+		public async Task SaveAsync()
+		{
+			await _context.SaveChangesAsync();
+		}
 
-        public void Update(Employee entity)
-        {
-            _context.Update(entity);
-        }
-    }
+		public void Update(Employee entity)
+		{
+			_context.Update(entity);
+		}
+	}
 }
